9a4084fa9044e47880ee03d9834fb29a3e5d7f6c
piano1029 <46605242+piano1029@users.noreply.github.com>
2022-07-25 20:40:20 +0000

    Add gateway notice

---
+++ docs/js/gateway_check.js
@@ -0,0 +1,5 @@
+
+let noticeObject = document.getElementById(`gateway_notice`)
+if (window.location.href.includes(`gateway=true`)) {
+    noticeObject.style = `width: 100%; background-color: #1f212e; padding: 20px; text-align: center;`
+}
