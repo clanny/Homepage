009f0ca77bb0d2317be9248e2feed40e0c39dbf2
piano1029 <46605242+piano1029@users.noreply.github.com>
2022-10-19 22:12:28 +0200

    update the z-index in the correct place

--- docs/js/gateway_check.js
+++ docs/js/gateway_check.js
@@ -1,5 +1,5 @@
 
 let noticeObject = document.getElementById(`gateway_notice`)
 if (window.location.href.includes(`gateway=true`)) {
-    noticeObject.style = `width: 100%; background-color: #1f212e; padding: 20px; text-align: center;`
+    noticeObject.style = `width: 100%; background-color: #1f212e; padding: 20px; text-align: center; z-index: 1000000;`
 }
